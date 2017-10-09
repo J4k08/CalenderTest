ec9af6d7aacaa175e0f8e21e081fb78aeced0dce
Jakob Haglöf <j.haglof56@gmail.com>
2017-10-09 10:38:05 +0200

    adds non-functional buttons + some simple layout

--- assets/js/gapi.js
+++ assets/js/gapi.js
@@ -27,12 +27,13 @@ angular.module('app', ['ngMaterial', 'ngRoute', 'ds.clock'])
     .controller('gapiController', function($scope, $document) {
 
         $scope.fetchedEvents = [];
-        $scope.tempEvents = [];
+        $scope.currentEvent = [];
+
     "use strict";
     var apiKey= {
         discoveryDocs: ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"],
         clientId: "725921814233-a7hb0cpvrb5rkiicubsead57322je7dc.apps.googleusercontent.com",
-        scope: "https://www.googleapis.com/auth/calendar.readonly"
+        scope: "https://www.googleapis.com/auth/calendar"
     };
     //var discovery_Docs = ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"];
     //var client_Id = "725921814233-a7hb0cpvrb5rkiicubsead57322je7dc.apps.googleusercontent.com";
@@ -40,7 +41,9 @@ angular.module('app', ['ngMaterial', 'ngRoute', 'ds.clock'])
     console.log($document);
     var authorizeButton = angular.element(document.querySelector("#authorize-button"));
     var signoutButton = angular.element(document.querySelector("#signout-button"));
-    var eventFetchedButton = angular.element(document.querySelector("#event-fetcher-button"));
+    var extendButton = angular.element(document.querySelector("#extendButton"));
+    var endEarlyButton = angular.element(document.querySelector("#endEarlyButton"));
+    var deleteButton = angular.element(document.querySelector("#deleteButton"));
 
     console.log("auth");
     gapi.load('client:auth2', initClient);
@@ -59,29 +62,30 @@ angular.module('app', ['ngMaterial', 'ngRoute', 'ds.clock'])
     function updateSignInStatus(isSignedIn) {
         console.log("updateSigninStatus");
         if (isSignedIn) {
-            //authorizeButton.css('display', 'none');
-            //signoutButton.css('display', 'block');
-            //listUpcomingEvents();
+
+
+            //enable get events knapp
+            authorizeButton.css('visibility', 'hidden');
+            signoutButton.css('visibility', 'visible');
+            listUpcomingEvents($scope.fetchedEvents);
         } else {
-            //authorizeButton.css('display', 'block');
-            //signoutButton.css('display', 'none');
+
+            authorizeButton.css('visibility', 'visible');
+            signoutButton.css('visibility', 'hidden');
         }
     }
 
     function handleAuthClick(event) {
         gapi.auth2.getAuthInstance().signIn();
-    }
-
-    function appendPre (message) {
-        console.log("message: " + message);
-        $scope.content = message + '\n';
+        console.log("handleAuthClick");
     }
 
     function handleSignoutClick(event) {
+        console.log("Signout");
         gapi.auth2.getAuthInstance().signOut();
     }
 
-    function listUpcomingEvents() {
+    function listUpcomingEvents(eventsArray) {
         gapi.client.calendar.events.list({
             'calendarId': 'primary',
             'timeMin': (new Date()).toISOString(),
@@ -91,34 +95,31 @@ angular.module('app', ['ngMaterial', 'ngRoute', 'ds.clock'])
             'orderBy': 'startTime'
         }).then(function(response) {
 
-            console.log(response.result.items);
             var events = response.result.items;
-            console.log("Events: " + events[0].creator.displayName);
-            appendPre('Upcoming events:');
-
             if (events.length > 0) {
                 for (var i = 0; i < events.length; i++) {
+                    (function(){
                     var event = events[i];
                     var when = event.start.dateTime;
                     if (!when) {
                         when = event.start.date;
                     }
-                    appendPre(event.summary + ' (' + when + ')');
                     $scope.$apply(function() {
-                        $scope.fetchedEvents.push(event);
+                        eventsArray[i] = event;
+                        console.log("Events added to array")
                     });
+                    })();
                 }
+                $scope.currentEvent = eventsArray[0];
+                $scope.displayButton(true);
 
             } else {
-                appendPre('No upcoming events found.');
+                $scope.clearEventsList();
             }
-            console.log("fetchedEvents: " + $scope.fetchedEvents);
             $scope.$apply(function() {
-                $scope.convertDateTime($scope.fetchedEvents);
+                $scope.convertDateTime(eventsArray);
             });
-
         });
-
     }
 
     $scope.convertDateTime = function(eventArray) {
@@ -133,23 +134,42 @@ angular.module('app', ['ngMaterial', 'ngRoute', 'ds.clock'])
             eventArray[i].start.startIso = startIso;
             eventArray[i].end.endIso = endIso;
         }
-        console.log("StartIso: " + startIso);
-        console.log("EndIso: " + endIso);
+    };
+    $scope.updateCalEvents = function() {
+
+        listUpcomingEvents($scope.fetchedEvents);
     };
 
-    $scope.getCalEvents = function() {
+    $scope.getCurrentEvent = function($event, event) {
 
-        if($scope.fetchedEvents.length === 0) {
-            listUpcomingEvents();
-            $scope.tempEvents = $scope.fetchedEvents;
+        $scope.currentEvent = event;
 
-        } else if ($scope.fetchedEvents.length > $scope.tempEvents.length) {
-            listUpcomingEvents();
+    };
+    $scope.clearEventsList = function() {
 
+        $scope.fetchedEvents = [];
+        $scope.currentEvent = [];
+        $scope.displayButton(false)
+    };
+
+    $scope.displayButton = function (bool) {
+
+        if(bool) {
+
+            console.log("true");
+            deleteButton.css('visibility', 'visible');
+            endEarlyButton.css('visibility', 'visible');
+            extendButton.css('visibility', 'visible');
         } else {
-            console.log("No new events");
+
+            console.log("false");
+            deleteButton.css('visibility', 'hidden');
+            endEarlyButton.css('visibility', 'hidden');
+            extendButton.css('visibility', 'hidden');
+
         }
     }
+
 });
     /*
      1: Skriv om gapi.js till angular                           [ Check ]
